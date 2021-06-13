78d825878bc51f3551abc0694084f7a5532c5880
Benjamin <you@example.com>
2021-06-13 15:04:53 +0300

    Added a "Private Channel" option

--- Jerubbaal-Bot.py
+++ Jerubbaal-Bot.py
@@ -1,3 +1,6 @@
+import inspect
+import string
+
 import discord
 from discord.ext import commands
 import asyncio
@@ -34,6 +37,8 @@ TOKEN_NAME = "JERUBBAAL_TOKEN"
 DEFAULT_CURRENTSONG = "song.mp3"
 PLAY_AUDIO_PATH = r"https://drive.google.com/uc?export=download&id="
 PLAY_AUDIO_FILES = {"alabama": PLAY_AUDIO_PATH + r"1Szlw3aQWPFzAc4EXjoDc1ZIARqtLFbj1",
+                    "arielogen": PLAY_AUDIO_PATH + r"1WdoQaXqjDdtqmLead9sOMBpNe_W0UhJD",
+                    "benjaminogen": PLAY_AUDIO_PATH + r"1FhQfa4pY33jslRoOBrRm6Tfs0xB02Wh9",
                     "bruh": PLAY_AUDIO_PATH + r"1CPnEc0_8P4yNbLacQHz7OLNPKqmyVUlW",
                     "cohen": PLAY_AUDIO_PATH + r"1DQALe7QpigJzeN6nFLnwJQsu2QFu9m0w",
                     "chupapi": PLAY_AUDIO_PATH + r"1oFRB0mk9ixb2LmcbEFlsCUSwbSc5sIEt",
@@ -41,13 +46,16 @@ PLAY_AUDIO_FILES = {"alabama": PLAY_AUDIO_PATH + r"1Szlw3aQWPFzAc4EXjoDc1ZIARqtL
                     "globglib": PLAY_AUDIO_PATH + r"1_9Em5QiOk6P3hQnrvsZfaUHqW_OfrORc",
                     "hellnaw": PLAY_AUDIO_PATH + r"11410Nl1lghF2XpA51fhsiYjwaM31CxZ3",
                     "heysisters": PLAY_AUDIO_PATH + r"18NxjwyaTsixirWkpmizCS_sRfYEe_EvE",
+                    "idoogen": PLAY_AUDIO_PATH + r"1WfgneI1Vcm5PMb47_0MENwsQfmsA5K_1",
                     "imgood": PLAY_AUDIO_PATH + r"1bIPGLBukEylJW-vOvNlIn-XHCVq9mFMT",
                     "moyal": PLAY_AUDIO_PATH + r"18fmO5glKSUbSbwX1kDLjWf216wPu6FiO",
                     "moyal2": PLAY_AUDIO_PATH + r"1oYslu8Ig27jq2bRQQMk1KjLdMIVn_aJq",
                     "moyal3": PLAY_AUDIO_PATH + r"1m8CsGICX2IQ91uqDQbdDdep6Oe_RhH2b",
                     "moyalrap": PLAY_AUDIO_PATH + r"1ubqLkIJxsmQxeQMl3cA9o6V62iUkkXXF",
                     "munyanyu": PLAY_AUDIO_PATH + r"1qYdWwqfrk3MAmFeYc_fwspWRH10p2KfN",
+                    "ofekogen": PLAY_AUDIO_PATH + r"1jhZkqLCmGEE6I70UZWzzMYZ_9KnSFh0x",
                     "purim": PLAY_AUDIO_PATH + r"1Wyrk7CqD0CszcjnazKCAwX2b7z-go4Pa",
+                    "royogen": PLAY_AUDIO_PATH + r"16J0KuGf2e_dxJcnPJQu1FH-dARlPmamj",
                     "shira": PLAY_AUDIO_PATH + r"1bn6s4VT5AtSDuunOxIc5uH1_JiwOlFuD",
                     "shonogen": PLAY_AUDIO_PATH + r"1yLofhA4pgNGzjvUtXzErwv5lj7E9aPcG",
                     "stepbro": PLAY_AUDIO_PATH + r"1fGk_Dg7KqCuzEsI1VcpLO7vCriB1eIKP",
@@ -56,9 +64,11 @@ PLAY_AUDIO_FILES = {"alabama": PLAY_AUDIO_PATH + r"1Szlw3aQWPFzAc4EXjoDc1ZIARqtL
 
 SEND_FILE_PATH = r"..\files" + "\\"
 SEND_FILE_FILES = {"ariel": SEND_FILE_PATH + r"ariel.png"}
+OWNER_ID = 0
 
 bot = commands.Bot(command_prefix=(f"jerubbaal ", "j ", "chupapi "), intents=discord.Intents.all())
 bot.remove_command("help")
+bot_owner = discord.User
 
 
 def error_str(err):
@@ -85,6 +95,18 @@ class Jerubbaal(commands.Cog):
         self.aliases = {}
         self.members_kicked = {}
 
+    @staticmethod
+    async def unhandled_error(ctx, error):
+        error_msg = f"Hey {bot_owner.name}!\n" \
+                    f"Sorry to bother you but I had an error :(\n" \
+                    f"Anyways, on the `{ctx.guild.name}` ({ctx.guild.id}) guild " \
+                    f"this dude `{ctx.author.name}` ({ctx.author.id}) used the command:\n" \
+                    f"```{ctx.message.content}```\n" \
+                    f"And I got an error of type `{type(error)}` which says:\n" \
+                    f"```{error}```\n" \
+                    f"{bot_owner.name}, pls fix"
+        await bot_owner.send(error_msg)
+
     @commands.command(aliases=["gn"], help="Disconnects everyone from your channel simultaneously")
     async def goodnight(self, ctx):
         if ctx.author.voice is None or ctx.author.voice.channel is None:
@@ -93,12 +115,49 @@ class Jerubbaal(commands.Cog):
         if len(vc_members) == 0:
             for k in ctx.author.voice.channel.voice_states.keys():
                 vc_members.append(await ctx.guild.fetch_member(k))
-        print([members.name for members in vc_members])
         for mem in vc_members:
             if mem is None:
                 continue
             await mem.edit(voice_channel=None)
         await ctx.message.add_reaction("😴")
+
+    @commands.command(aliases=["pc"], help="Creates a private channel just for you and who you tagged!")
+    async def privatechannel(self, ctx, *members: discord.Member):
+        if len(members) == 0:
+            raise commands.MissingRequiredArgument(inspect.Parameter("member", inspect.Parameter.POSITIONAL_ONLY))
+        overwrites = {ctx.guild.default_role: discord.PermissionOverwrite(view_channel=False)}
+        for member in members:
+            overwrites[member] = discord.PermissionOverwrite(view_channel=True)
+        channel_name = f"{ctx.author.nick}'s private channel"
+        for p in string.punctuation:
+            channel_name = channel_name.replace(p, "")
+        channel_name = channel_name.replace(" ", "-").lower()
+        if channel_name in [c.name for c in ctx.guild.text_channels]:
+            await ctx.message.reply(error_str("You already have a private channel!"))
+            return
+        new_channel = await ctx.guild.create_text_channel(channel_name, overwrites=overwrites)
+        welcome_message = await new_channel.send(f"Welcome to <@{ctx.author.id}>'s private channel with "
+                                                 f"{','.join([f'<@{m.id}>' for m in members])}!\nYou have 5 minutes!")
+        await welcome_message.add_reaction("✅")
+
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) == "✅"
+
+        try:
+            reaction, user = await bot.wait_for('reaction_add', timeout=300.0, check=check)
+        except asyncio.TimeoutError:
+            await new_channel.delete()
+        else:
+            await new_channel.delete()
+
+    @privatechannel.error
+    async def privatechannel_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.message.reply(error_str("you must have at least one member"))
+        elif isinstance(error, commands.MemberNotFound):
+            await ctx.message.reply(error_str("member not found"))
+        else:
+            await Jerubbaal.unhandled_error(ctx, error)
 
     @commands.command(aliases=["pa", "play"], help="Plays an audio from a predetemined list",
                       name="playaudio", usage="<sound> [repeat]")
@@ -138,11 +197,12 @@ class Jerubbaal(commands.Cog):
             await channel.connect()
 
         self.audio = audio
+        print("Writing")
         with open(audio + ".mp3", "wb") as f:
             f.write(requests.get(PLAY_AUDIO_FILES[audio]).content)
+        print("Playing")
         length = audio_len(audio + ".mp3")
         for _ in range(repeat):
-            # ctx.guild.voice_client.play(discord.FFmpegPCMAudio(PLAY_AUDIO_FILES[audio]))
             ctx.guild.voice_client.play(discord.FFmpegPCMAudio(audio + ".mp3"))
             self.playing = True
             await asyncio.sleep(length)
@@ -151,7 +211,7 @@ class Jerubbaal(commands.Cog):
         self.playing = False
         if ctx.guild.voice_client is not None:
             await ctx.guild.voice_client.disconnect()
-        await ctx.message.add_reaction("👍🏻")
+        await ctx.message.add_reaction("🎶")
 
     @play_audio.error
     async def play_audio_error(self, ctx, error):
@@ -159,8 +219,7 @@ class Jerubbaal(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.message.reply(error_str("missing audio file!"))
         else:
-            print(error)
-            print(ctx)
+            await Jerubbaal.unhandled_error(ctx, error)
             if ctx.guild.voice_client is not None:
                 await ctx.guild.voice_client.disconnect()
             if os.path.isfile(self.audio + ".mp3"):
@@ -173,7 +232,6 @@ class Jerubbaal(commands.Cog):
             await ctx.message.reply(error_str("amount must be an integer between 1 and 100"))
             return
         await ctx.channel.purge(limit=limit+1, check=lambda msg: msg != ctx.message)
-        # await ctx.send(f"Cleared {limit} messages!")
         await ctx.message.add_reaction("👍🏻")
 
     @clear.error
@@ -183,7 +241,7 @@ class Jerubbaal(commands.Cog):
         elif isinstance(error, commands.BadArgument):
             await ctx.message.reply(error_str("amount must be an integer"))
         else:
-            print(error)
+            await Jerubbaal.unhandled_error(ctx, error)
 
     @commands.command(alias=["undeafen", "ud"], usage="<member>", help="Move the player around 8 times")
     async def undeaf(self, ctx, member: discord.Member):
@@ -192,8 +250,7 @@ class Jerubbaal(commands.Cog):
             return
         if ctx.author.voice is None or ctx.author.voice.channel is None or \
                 ctx.author.voice.channel != member.voice.channel:
-            print(ctx.author.voice.channel)
-            print(member.voice.channel)
+
             await ctx.message.reply(error_str("you must be in the same channel with the target"))
             return
         if not member.voice.self_deaf:
@@ -203,7 +260,7 @@ class Jerubbaal(commands.Cog):
         orig_channel = member.voice.channel
         channels = ctx.guild.voice_channels[:len(ctx.guild.voice_channels) // 2]
         for channel in channels:
-            if len(channel.members) != 0 or len(channel.voice_states) != 0 or channel == member.voice.channel or \
+            if len(channel.members) != 0 or len(channel.voice_states) != 0 or channel == orig_channel or \
                     discord.Permissions.connect in channel.permissions_for(member):
                 del channel
 
@@ -221,8 +278,7 @@ class Jerubbaal(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.message.reply(error_str("please specify member"))
         else:
-            print(type(error))
-            print(error)
+            await Jerubbaal.unhandled_error(ctx, error)
 
     @commands.command(name="sendfile", aliases=["sf"], usage="<file>", help="Sends a file from a predetemined list")
     async def send_file(self, ctx, file):
@@ -244,8 +300,7 @@ class Jerubbaal(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.message.reply(error_str("missing file!"))
         else:
-            print(type(error))
-            print(error)
+            await Jerubbaal.unhandled_error(ctx, error)
 
     @commands.command(usage="<member>", help="Kicks a member, send them (privately) an invite link, and when they "
                                              "return their roles and nickname will be as of before the kick")
@@ -259,8 +314,6 @@ class Jerubbaal(commands.Cog):
         roles = member.roles
         nick = member.nick
         self.members_kicked[(member.id, ctx.guild.id)] = (nick, roles[1:])
-        with open("..\\guilddata\\kicked_" + str(ctx.guild.id) + ".txt", "w") as f:
-            f.write(json.dumps(self.members_kicked))
         inv = await ctx.guild.text_channels[0].create_invite(max_uses=1)
         await member.send(content=f"Apparently, {ctx.author.name} kicked you. Come back through here: {inv.url}")
         await ctx.guild.kick(member)
@@ -271,10 +324,9 @@ class Jerubbaal(commands.Cog):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.message.reply(error_str("missing target!"))
         elif isinstance(error, commands.MemberNotFound):
-            await ctx.message.reply(error_str("memeber not found"))
+            await ctx.message.reply(error_str("member not found"))
         else:
-            print(type(error))
-            print(error)
+            await Jerubbaal.unhandled_error(ctx, error)
 
     @commands.command(usage="[length] <alias> <command>", help="Adds an alias to a command. Length defaults to 1. "
                                                                "To list the aliases, supply no arguments")
@@ -305,7 +357,7 @@ class Jerubbaal(commands.Cog):
             f.write(json.dumps(self.aliases))
         await ctx.message.add_reaction("👍🏻")
 
-    @commands.command(usage="<alias>", help="Delete an alias")
+    @commands.command(usage="<alias>", help="Delete an alias", name="deletealias", aliases=["delalias"])
     async def delalias(self, ctx, *args):
         if os.path.exists("..\\guilddata\\aliases_" + str(ctx.guild.id) + ".txt"):
             with open("..\\guilddata\\aliases_" + str(ctx.guild.id) + ".txt", "r") as f:
@@ -328,8 +380,8 @@ class Jerubbaal(commands.Cog):
                 ctx.message.content = self.aliases[" ".join(args)]
                 await self.bot.process_commands(ctx.message)
         else:
-            print(type(error))
-            print(error)
+            # await Jerubbaal.unhandled_error(ctx, error)
+            pass
 
     @commands.command(help="Shows this message")
     async def help(self, ctx):
@@ -351,12 +403,12 @@ class Jerubbaal(commands.Cog):
             await member.add_roles(*roles)
             await member.edit(nick=nick)
             self.members_kicked.pop((member.id, member.guild.id))
-            with open("..\\guilddata\\kicked_" + str(member.guild.id) + ".txt", "w") as f:
-                f.write(json.dumps(self.members_kicked))
 
     @commands.Cog.listener()
     async def on_connect(self):
+        global bot_owner
         print(f"Connected as {self.bot.user}")
+        bot_owner = await bot.fetch_user(OWNER_ID)
 
 
 token = ""
@@ -367,6 +419,12 @@ with open(r"..\tokens.txt", "r") as tokens:
             token = splitted[1]
 if token == "":
     print("File tokens.txt not found!")
+    exit(1)
+
+with open(r"..\owner_id.txt", "r") as ownerid:
+    OWNER_ID = int(ownerid.read())
+if OWNER_ID == 0:
+    print("File owner_id.txt not found!")
     exit(1)
 
 jerubbaal_cog = Jerubbaal(bot)
